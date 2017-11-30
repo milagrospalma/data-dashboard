c2b9e09aa99e265c694fb5833f133eead543d308
MariaRuizQ <ruiz.maria.quispe@gmail.com>
2017-11-30 17:00:02 -0500

    añadiendo más estructura JS

--- js/app.js
+++ js/app.js
@@ -1,4 +1,70 @@
 window.addEventListener('load',function(){
+  //realizando el evento click para el button general Arequipa 2016-2
+  var overview = document.getElementById('overview');
+  var overviewBtn = document.getElementById('overview-btn')
+
+  overviewBtn.addEventListener('click', function(){
+    var div2 = document.createElement('div');
+    overview.appendChild(div2);
+    //for para el NPS
+    //jalamos los ratings de Arequipa 2016-2, mediante la data
+    var overviewAqp = data.AQP['2016-2'].ratings
+    for (var i = 0; i < overviewAqp.length; i++){
+      var overviewAqp2016a = overviewAqp[i];
+      var promotersOverviewAqp2016a = overviewAqp2016a.nps.promoters;
+      var passiveOverviewAqp2016a = overviewAqp2016a.nps.passive;
+      var detractorsOverviewAqp2016a = overviewAqp2016a.nps.detractors;
+      var sum = (promotersOverviewAqp2016a + passiveOverviewAqp2016a + detractorsOverviewAqp2016a)
+      var promoters = promotersOverviewAqp2016a/sum*100
+      var detractors = detractorsOverviewAqp2016a/sum*100
+      var nps = promoters - detractors
+      //creando los elementos
+      var pNPS = document.createElement('p');
+      //añadiendo el contenido a los elementos creados
+      pNPS.textContent = 'NPS' + ':' + ' ' + nps;
+      //añadiendo los nodos
+      div2.appendChild(pNPS);
+      //agregando los atributos
+      pNPS.setAttribute('class','overviewNPS');
+    }
+    //for para La puntuación promedio de l@s profesores.
+    for (var i = 0; i < overviewAqp.length; i++){
+      var overviewAqp2016a = overviewAqp[i];
+      var teacheroverviewAqp2016a = overviewAqp2016a.teacher;
+      //creando los elementos
+      var pPuntsTeacher = document.createElement('p')
+      //añadiendo el contenido a los elementos creados
+      pPuntsTeacher.textContent = 'PUNTACIÓN PROMEDIO PROFESORES' + ':' + ' ' + teacheroverviewAqp2016a;
+      //añadiendo los nodos
+      div2.appendChild(pPuntsTeacher)
+      //agregando los atributos
+      pPuntsTeacher.setAttribute('class','overviewTEACHER')
+    }
+    //for para La puntuación promedio de l@s jedi.
+    for (var i = 0; i < overviewAqp.length; i++){
+      var overviewAqp2016a = overviewAqp[i];
+      var jedioverviewAqp2016a = overviewAqp2016a.jedi;
+      //creando los elementos
+      var pPuntsJedi = document.createElement('p')
+      //añadiendo el contenido a los elementos creados
+      pPuntsJedi.textContent = 'PUNTACIÓN PROMEDIO JEDI' + ':' + ' ' + jedioverviewAqp2016a
+      //añadiendo los nodos
+      div2.appendChild(pPuntsJedi)
+      //agregando los atributos
+      pPuntsJedi.setAttribute('class','overviewJEDI')
+    }
+    //for para el total de estudiantes presentes
+    var overviewAqp2 = data.AQP['2016-2'].students
+    var contador = 0;
+    for (var i = 0; i < overviewAqp2.length; i++){
+      var overviewAqp2a = overviewAqp2[i];
+      var activeOverviewAqp2a = overviewAqp2a.active
+      if (activeOverviewAqp2a == true){
+        console.log(contador++)
+      }
+    }
+    //for (var i = 0; i < overviewAqp2.length; i++){
+  });
   //realizando el evento click para el button estudiantes Arequipa 2016-2
   var students = document.getElementById('students');
   var studentsBtn = document.getElementById('students-btn')
@@ -68,53 +134,5 @@ window.addEventListener('load',function(){
       }
   })
 
-  //realizando el evento click para el button general Arequipa 2016-2
-
-  var overview = document.getElementById('overview');
-  var overviewBtn = document.getElementById('overview-btn')
-
-  overviewBtn.addEventListener('click', function(){
-    var div2 = document.createElement('div');
-    overview.appendChild(div2);
-    //for para el NPS
-    var intento2 = data.AQP['2016-2'].ratings
-    for (var i = 0; i < intento2.length; i++){
-
-      var x2 = intento2[i];
-      var y2 = x2.nps.promoters;
-      var z2 = x2.nps.passive;
-      var w2 = x2.nps.detractors;
-      var sumXZW = (y2 + z2 + w2)
-      var promoters = y2/sumXZW*100
-      var detractors = w2/sumXZW*100
-      var nps = promoters - detractors
-      var p3 = document.createElement('p')
-      p3.textContent = 'NPS' + ':' + nps;
-      div2.appendChild(p3)
-    /*  [Promoters] = [Respuestas 9 o 10] / [Total respuestas] * 100
-  [Passive] = [Respuestas 7 u 8] / [Total respuestas] * 100
-  [Detractors] = [Respuestas entre 1 y 6] / [Total respuestas] * 100
-  [NPS] = [Promoters] - [Detractors] */
-    }
-
-    //for para La puntuación promedio de l@s profesores.
-    for (var i = 0; i < intento2.length; i++){
-
-      var x3 = intento2[i];
-      var y3 = x3.teacher;
-      var p4 = document.createElement('p')
-      p4.textContent = 'PUNTACIÓN PROMEDIO PROFESORES' + ':' + y3
-      div2.appendChild(p4)
-    }
-    //for para La puntuación promedio de l@s jedi.
-    for (var i = 0; i < intento2.length; i++){
-
-      var x4 = intento2[i];
-      var y4 = x4.jedi;
-      var p5 = document.createElement('p')
-      p5.textContent = 'PUNTACIÓN PROMEDIO JEDI' + ':' + y4
-      div2.appendChild(p5)
-    }
-    });
 });
 console.log (data)
