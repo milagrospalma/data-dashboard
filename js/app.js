ab161829a8d8b3b1369b10dbbc8cc6e42b890d59
milagrospalma <mnpalma.2@gmail.com>
2017-12-04 12:58:00 -0500

    Añade vista general al seleccionar una sede

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ window.addEventListener('load', function() {
   var overviewBtn = document.getElementById('overview-btn');
   var students = document.getElementById('students');
   var studentsBtn = document.getElementById('students-btn');
+  var enrollment = document.getElementById('enrollment');
+
   // Cargando data al selector de Sedes
   var arrHeadquarter = Object.keys(data);
   // Creando un Array vacío para almacenar las sedes
@@ -42,7 +44,7 @@ window.addEventListener('load', function() {
   var greet = document.createElement('p');
 
   // Evento para cargar selector de Generaciones
-  selectHeadquarters.addEventListener('change', function() {
+  selectHeadquarters.addEventListener('change', function(event) {
     // Función para agregar opciones en selector Generación
     function addOptions(arrHeadquarter) {
       var objHeadquarters = data[arrHeadquarter];
@@ -57,35 +59,41 @@ window.addEventListener('load', function() {
       }
       // console.log(arrOptionGeneration); // [1, 0]
     }
-    // Creando función para agregar overview por defecto
-    function addOverviewDefault() {
 
-    }
     // Creando variable que almacena el índice de la opción seleccionada
     var selectedHeadquarter = selectHeadquarters.selectedIndex;
     if (selectedHeadquarter != 0) {
       backgroundDefault.style.display = 'none';
       overviewBtn.removeAttribute('disabled');
       studentsBtn.removeAttribute('disabled');
+      //
       switch (true) {
       case selectedHeadquarter === 1:
         // Desaparece el texto "Generación" del select y muestra una generación
         selectGeneration.options.length = 0;
         addOptions('AQP');
-        greet.textContent = 'Aquí debe mostrarse la info general porque una vez seleccionada la sede, se selecciona una generacion por defecto';
-        overview.appendChild(greet);
+        // Creando variable que almacene el valor de la última generación
+        var lastGeneration = selectGeneration.value; // devuelve 0 para todos
+        console.log(lastGeneration);
+        addOverviewDefault('AQP', lastGeneration);
         break;
       case selectedHeadquarter === 2:
         selectGeneration.options.length = 0;
         addOptions('CDMX');
+        var lastGeneration = selectGeneration.value;
+        addOverviewDefault('CDMX', lastGeneration);
         break;
       case selectedHeadquarter === 3:
         selectGeneration.options.length = 0;
         addOptions('LIM');
+        var lastGeneration = selectGeneration.value;
+        addOverviewDefault('LIM', lastGeneration);
         break;
       case selectedHeadquarter === 4:
         selectGeneration.options.length = 0;
         addOptions('SCL');
+        var lastGeneration = selectGeneration.value;
+        addOverviewDefault('SCL', lastGeneration);
         break;
       }
     } else {
@@ -99,7 +107,103 @@ window.addEventListener('load', function() {
       overviewBtn.setAttribute('disabled', true);
       studentsBtn.setAttribute('disabled', true);
     }
+
+    // Creando función para agregar overview por defecto
+    function addOverviewDefault(nameHeadquarter, lastGeneration) {
+      var arrNameGeneration = Object.keys(data[nameHeadquarter]);
+      // console.log(arrGeneraciones);
+      for (var i = 0; i < arrNameGeneration.length; i++) {
+        if (arrNameGeneration[i] === lastGeneration) {
+          var arrStudents = data[nameHeadquarter][lastGeneration].students;
+        }
+      }
+      // console.log(arrStudents);
+      // console.log(arrStudents.length);
+
+      var contGoal = 0;// que alcanzaron la meta
+      var contTech = 0;
+      var contHse = 0;
+      var qTech;
+      var uHse;
+      var contInactive = 0;
+
+      for (var x = 0; x < arrStudents.length; x++) {
+        if (arrStudents[x].active === false) {
+          contInactive++;
+        }
+        // empezando los calculos cuando el alumno está activo
+        if (arrStudents[x].active === true) {
+          var sumTech = 0;
+          var sumHse = 0;
+
+          for (var y = 0; y < arrStudents[x].sprints.length; y++) { // va a sumar el puntaje total de los sprints-alumna en tech y hse
+            sumTech += arrStudents[x].sprints[y].score.tech;
+            // console.log(arrStudents[k].sprints[y].score.tech);//ingresa a la nota por número de sprint del alumno
+            sumHse += arrStudents[x].sprints[y].score.hse;
+            // console.log(arrStudents[k].sprints[y].score.hse);
+          }
+          // console.log(sumTech);//suma total de notas en tech de cada alumno de la generación elegida
+          // console.log(sumHse);//suma total de notas en tech de cada alumno de la generación elegida
+          // console.log(arrStudents[k].sprints)//me muestra los sprints del alumno
+
+          // Cálculos para los promedios por generación escogida
+          qTech = sumTech / arrStudents[x].sprints.length;// me muestra el promedio de las notas tech (sumTech/#sprints llevados)
+          uHse = sumHse / arrStudents[x].sprints.length;// me muestra el promedio de las notas hse (sumHse/#sprints llevados)
+          // console.log(qTech);//promedio de tech(de c/alumno)
+          // console.log(arrStudents[k].sprints);//número de sprints(de c/alumno)
+
+          if (uHse >= 840 && qTech >= 1240) {
+            contGoal++;
+          }
+          if (qTech >= 1240) {
+            contTech++;
+          }
+          if (uHse >= 840) {
+            contHse++;
+          }
+        }
+      }
+      var retired = ((contInactive * 100) / arrStudents.length).toFixed(1);
+      var studentAchievement = ((contGoal * 100) / arrStudents.length).toFixed(1);
+      var neoTech = ((contTech / (arrStudents.length - contInactive)) * 100).toFixed(1);
+      var neoSkills = ((contHse / (arrStudents.length - contInactive)) * 100).toFixed(1);
+
+      // NPS
+      for (var i = 0; i < arrNameGeneration.length; i++) {
+        if (arrNameGeneration[i] === lastGeneration) {
+          var arrRatings = data[nameHeadquarter][lastGeneration].ratings;
+        }
+      }
+      console.log(arrRatings)//obtengo a todos los ratings de la generación elegida
+
+      var detractors = 0;
+      var promoters = 0;
+      var pasive = 0;
+
+      for (var z = 0; z < arrRatings.length; z++) {
+        detractors += arrRatings[z].nps.detractors;
+        promoters += arrRatings[z].nps.promoters;
+        pasive += arrRatings[z].nps.passive;
+      }
+      var resultDetractors = (detractors / arrRatings.length).toFixed(1);
+      var resultPromoters = (promoters / arrRatings.length).toFixed(1);
+      var resultPasive = (pasive / arrRatings.length).toFixed(1);
+      // ya no se multiplicó por 100, ya que el 100% es equivalente a la unidad
+      var resultNps = resultPromoters - resultDetractors;
+
+      // enrollment.textContent = '';
+      enrollment.textContent = arrStudents.length;
+      var retiredAlumn = document.getElementById('retiredAlumn');
+      // retiredAlumn.textContent = '';
+      retiredAlumn.textContent = retired;
+
+      // greet.textContent = 'Muestra vista general de ultima gen';
+      // overview.appendChild(greet);
+
+    }
+
   });
+
 
   // Evento para cargar data al seleccionar una generación
   selectGeneration.addEventListener('change', function() {
@@ -109,7 +213,7 @@ window.addEventListener('load', function() {
     // for (var i = 0; i < arrOptionGeneration.length; i++) {
     //   var selectedGeneration = arrOptionGeneration[i];
     // }
-    // console.log('Index de la opción: ' + selectedGeneration);
+     console.log('value de la opción: ' + selectGeneration.value);
   });
   // Evento para el boton general
   overviewBtn.addEventListener('click', function() {
